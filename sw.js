6be9e1bc505ef23faf939ae9db9b10a4cdc2fbdd
Pexcci <Pexcci@users.noreply.github.com>
2026-08-17 21:48:17 +0800

    Refresh cached site styles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "zhiqiang-home-v6";
+const CACHE_NAME = "zhiqiang-home-v7";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -37,10 +37,10 @@ self.addEventListener("fetch", (event) => {
     return;
   }
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    fetch(event.request).then((response) => {
       const copy = response.clone();
       caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
       return response;
-    }))
+    }).catch(() => caches.match(event.request))
   );
 });
